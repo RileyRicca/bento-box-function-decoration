33f8c57a210274de0e48ea5378c2f7db568b03b1
RileyRicca <148147216+RileyRicca@users.noreply.github.com>
2023-12-01 21:00:18 +0000

    look on GPT conversation to finish the function.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-def Kill_the_Maze:
- print("foo")
+import os
+
+kill_code = ["float", "catch", "bleed", "death", "stiff", "push"]
+
+while True:
+    user_input = input(" ")
+
+if user_input in kill_code:
+    os.system('cls' if os.name == 'nt' else 'clear')
